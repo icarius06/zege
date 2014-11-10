0caf8cd7fb1ae7c290125f98d8c86dab613780e0
Michael <michael@knossos.(none)>
2014-11-10 15:03:27 +0300

    message

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,7 @@ module RailsDevisePundit
       g.fixture_replacement :factory_girl, dir: "spec/factories"
     end
 
+    config.serve_static_assets = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
