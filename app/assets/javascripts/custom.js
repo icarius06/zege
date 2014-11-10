c8c20cd78a763b56d63551b496d7ec684d787ec2
Michael <michael@knossos.(none)>
2014-11-10 15:10:07 +0300

    m

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -4,24 +4,28 @@
  *posts to the create function in the followeres controller
  *
  */
-$(document).ready(function() {
-	/*
-	changes actions based on controller 
-    */
-  $("#page_path").change(function(e){
-  	e.preventDefault();
-    
-  	var controller_name = $(this).val().split('#')[0];
-  	var action_name = $(this).val().split('#')[1];
 
-  	if (action_name=="new"){
-  		alert("Users in this level will be able to add "+action_name+" "+controller_name);	
-  	}
-  	else{
-  		alert("Users in this level will be able to "+action_name+" "+controller_name);
-  	}
-	
-  	$("#page_controller_name").val(controller_name)
-  	$("#page_action_name").val(action_name)
-  });
-});
+
+(function($) {
+    $(document).ready(function() {
+        /*
+      changes actions based on controller 
+        */
+      $("#page_path").change(function(e){
+        e.preventDefault();
+        
+        var controller_name = $(this).val().split('#')[0];
+        var action_name = $(this).val().split('#')[1];
+
+        if (action_name=="new"){
+          alert("Users in this level will be able to add "+action_name+" "+controller_name);  
+        }
+        else{
+          alert("Users in this level will be able to "+action_name+" "+controller_name);
+        }
+      
+        $("#page_controller_name").val(controller_name)
+        $("#page_action_name").val(action_name)
+      });
+    });
+}) (jQuery);
